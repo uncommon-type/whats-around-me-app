19ed33aac50f2861970fae8f35c04a54ec9d833f
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-08-06 11:57:09 +0300

    Display Map
    - Render Google Map
    - Add map styling

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,32 @@
 import React from 'react';
 
-const Map = () => <h1>Lorem Ipsum</h1>;
+import GoogleMapReact from 'google-map-react';
+
+const Map = () => {
+  const defaultProps = {
+    center: {
+      lat: 48.8566,
+      lng: 2.3522,
+    },
+    zoom: 16,
+  };
+
+  return (
+    <>
+      <h1>Lorem Ipsum</h1>
+      <div className="section__content">
+        <div className="map">
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: 'AIzaSyCbfV0IAdkkGv-9mmuAkUJNzCPPfGRO6v0',
+            }}
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Map;
