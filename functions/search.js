8a1955cdc12187e1beec7cf5a57a9f745f617f20
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-08-06 12:05:17 +0300

    Add a call to nearby API, mark response as json, return fetched data

--- functions/search.js
+++ functions/search.js
@@ -1,8 +1,22 @@
 const fetch = require('node-fetch');
 
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    body: '',
-  };
+const fetchFromWiki = async (lat, lng) => {
+  let url = `https://en.m.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=coordinates|pageimages|description&colimit=max&generator=geosearch&ggsradius=10000&ggsnamespace=0&ggslimit=50&ggscoord=${lat}|${lng}&piprop=thumbnail&pithumbsize=500&pilimit=50&codistancefrompoint=${lat}|${lng}`;
+
+  const res = await fetch(url);
+  return res.json();
+};
+
+exports.handler = async ({ queryStringParameters }) => {
+  let { lat, lng } = queryStringParameters;
+
+  try {
+    let data = await fetchFromWiki(lat, lng);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    console.log(error);
+  }
 };
