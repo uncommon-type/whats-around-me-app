f4df11126d8eeef380ea982ed4654291316008c3
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-08-06 16:51:29 +0300

    Add location sharing
    - Add button prompting user to share their location
    - Watch user’s position
    - Show user as a marker on the map
    - Fetch new pins based on new map center and update map
    - Add debounce functionality to avoid making multiple API calls
    - Add stop location sharing functionality to unregister handler
    watching user position
    - Remove marker showing user location on the map
    - Show Paris as a default view after user stops sharing location

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,18 +1,34 @@
-import React, { useState, useContext, useCallback } from 'react';
+import React, { useState, useContext, useCallback, useEffect } from 'react';
 
 import GoogleMapReact from 'google-map-react';
 import { Dialog } from '@reach/dialog';
 import '@reach/dialog/styles.css';
 
 import { FetchContext } from '../../contexts/FetchContextProvider';
+import useDebounce from '../../hooks/useDebounce';
 
 import Pin from './Pin';
 import DialogBody from '../Dialog/DialogBody';
+import UserMarker from './UserMarker';
 
 const Map = () => {
   const [loaded, setLoaded] = useState(false);
   const [locationDetails, setLocationDetails] = useState(null);
+  const [userPosition, setUserPosition] = useState({ lat: null, lng: null });
+  const [watcherId, setWatcherId] = useState(null);
+  const [isWatchingLocation, setIsWatchingLocation] = useState(false);
+  const [currentMapCenter, setCurrentMapCenter] = useState({
+    lat: 48.8566,
+    lng: 2.3522,
+  });
   const { data, centerCoords, setCenterCoords } = useContext(FetchContext);
+  const debouncedUserPosition = useDebounce(userPosition, 1000);
+
+  useEffect(() => {
+    if (debouncedUserPosition) {
+      setCenterCoords(debouncedUserPosition);
+    }
+  }, [debouncedUserPosition, setCenterCoords]);
 
   const { zoom } = data;
 
@@ -35,17 +51,62 @@ const Map = () => {
     [setCenterCoords],
   );
 
+  const onSuccess = ({ coords }) => {
+    const { latitude, longitude } = coords;
+    setUserPosition({ lat: latitude, lng: longitude });
+    setCurrentMapCenter({ lat: latitude, lng: longitude });
+  };
+
+  const onError = (error) => {
+    //TODO:Handle permission-denied/position-unavailale
+    console.log(error);
+  };
+
+  const handleSharing = () => {
+    const watcherId = navigator.geolocation.watchPosition(onSuccess, onError, {
+      enableHighAccuracy: true,
+    });
+
+    setWatcherId(watcherId);
+    setIsWatchingLocation(true);
+  };
+
+  const handleStopSharing = () => {
+    if (isWatchingLocation && watcherId) {
+      navigator.geolocation.clearWatch(watcherId);
+    }
+
+    setIsWatchingLocation(false);
+    setCenterCoords({ lat: 48.8566, lng: 2.3522 });
+  };
+
   return (
     <>
-      <h1>Lorem Ipsum</h1>
+      <header>
+        <h1>Lorem Ipsum</h1>
+        <button
+          onClick={handleSharing}
+          disabled={isWatchingLocation}
+          className="share-btn"
+        >
+          Share Location
+        </button>
+
+        {isWatchingLocation ? (
+          <button onClick={handleStopSharing} className="stop-btn">
+            Stop Sharing Location
+          </button>
+        ) : null}
+      </header>
       <div className="section__content">
         <div className="map">
           <GoogleMapReact
             bootstrapURLKeys={{
               key: 'AIzaSyCbfV0IAdkkGv-9mmuAkUJNzCPPfGRO6v0',
             }}
-            center={centerCoords}
+            center={currentMapCenter}
             defaultZoom={zoom}
+            yesIWantToUseGoogleMapApiInternals={true}
             onGoogleApiLoaded={handleApiLoaded}
             onDragEnd={(map) => handleDrag(map)}
           >
@@ -69,6 +130,10 @@ const Map = () => {
                   );
                 })
               : null}
+
+            {userPosition ? (
+              <UserMarker lat={userPosition.lat} lng={userPosition.lng} />
+            ) : null}
           </GoogleMapReact>
           {locationDetails ? (
             <Dialog
---
+++ src/components/Map/UserMarker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import UserIcon from './UserIcon';
+
+const UserMarker = () => (
+  <div className="user-icon">
+    <UserIcon />
+  </div>
+);
+
+export default UserMarker;
