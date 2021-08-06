9e62de415c13e0f0be26360a953c8b78e7065efa
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-08-06 12:28:34 +0300

    Render Fetched Data in a List
    - Add Card component
    - Display fetched data in a card list
    - Add list styling

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-const CardList = () => <h1>List</h1>;
+import { FetchContext } from '../contexts/FetchContextProvider';
+
+import Card from './Card';
+
+const CardList = () => {
+  const { data } = useContext(FetchContext);
+
+  return (
+    <>
+      <h1 className="section__title">List</h1>
+      <ul className="section__content list">
+        {data?.query?.pages?.length > 0
+          ? data.query.pages.map((location) => (
+              <Card key={location.pageid} location={location} />
+            ))
+          : null}
+      </ul>
+    </>
+  );
+};
 
 export default CardList;
