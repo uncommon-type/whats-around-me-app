91734db89e24f4bc6ea4b7444bd9084487ee11a1
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-09-18 17:36:22 +0300

    Allow users to set a default view (map or list)
    -On the settings view, add radio buttons letting users select their
    default view
    - Add ability to choose between map and list views as their defaults
    - Add useLocalStorage, make default view selection persist between app
    launches

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import FetchContextProvider from './contexts/FetchContextProvider';
 import GoogleContextProvider from './contexts/GoogleContextProvider';
 import ShareLocationContextProvider from './contexts/ShareLocationContextProvider';
+import useLocalStorage from './hooks/useLocalStorage';
 
+import Logo from './common/components/Logo';
 import Settings from './SettingsView/components/Settings';
 import Footer from './common/components/Footer';
 import SearchRoutes from './SearchRoutes';
@@ -12,6 +14,18 @@ import SearchRoutes from './SearchRoutes';
 import './App.css';
 
 const Root = () => {
+  const [view, setView] = useLocalStorage('view', 'Map');
+
+  const handleSelectView = (view) => {
+    setView(view);
+  };
+
+  useEffect(() => {
+    if (view === 'List' && window.location.pathname === '/') {
+      window.history.replaceState({}, '', '/list');
+    }
+  }, [view]);
+
   return (
     <FetchContextProvider>
       <GoogleContextProvider>
@@ -20,9 +34,10 @@ const Root = () => {
             <div className="app">
               <main>
                 <div className="app__inner">
+                  <Logo />
                   <Switch>
                     <Route path="/settings">
-                      <Settings />
+                      <Settings onViewChange={handleSelectView} view={view} />
                       <Footer />
                     </Route>
                     <Route component={SearchRoutes} />
--- src/SettingsView/components/Settings.js
+++ src/SettingsView/components/Settings.js
@@ -4,7 +4,12 @@ import ReactSlider from 'react-slider';
 
 import { FetchContext } from '../../contexts/FetchContextProvider';
 
-const Settings = () => {
+const VIEWS = [
+  { name: 'Map', id: '01' },
+  { name: 'List', id: '02' },
+];
+
+const Settings = ({ onViewChange, view }) => {
   const { apiTimeDelay, setApiTimeDelay } = useContext(FetchContext);
 
   const handleChange = (value) => {
@@ -55,6 +60,36 @@ const Settings = () => {
                 To save it, ajust how often the app refreshes with new data.
               </p>
               <p>Make adustments based on how you commute.</p>
+            </div>
+
+            <div className="instruction">
+              <header className="instruction__header">
+                <div>
+                  <span>2</span>
+                  <h3>Managing your app's views</h3>
+                </div>
+              </header>
+              <p>Select the view you want to set as your default.</p>
+              <form>
+                <ul className="default-view-options">
+                  {VIEWS.map((item) => {
+                    const { id, name } = item;
+                    return (
+                      <li key={id}>
+                        <input
+                          type="radio"
+                          id={name}
+                          name={name}
+                          value={name}
+                          checked={view === name}
+                          onChange={() => onViewChange(name)}
+                        />
+                        <label htmlFor={name}>{name}</label>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </form>
             </div>
           </div>
         </article>
