eb8a09b2a605da196d51e902dae5ebd0a6edd110
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-09-18 15:42:02 +0300

    Remove redundant svg

--- src/components/Map/Pin.js
+++ src/components/Map/Pin.js
@@ -3,10 +3,6 @@ import React from 'react';
 const Pin = ({ imageUrl, alt, onClick }) => (
   <div className="map-icon bounce" onClick={onClick} aria-hidden={true}>
     <img src={imageUrl} alt={alt} />
-    <svg viewBox="0 0 32 32" aria-labelledby="caret-icon">
-      <title id="caret-icon">Caret Icon</title>
-      <path fill="currentColor" d="M6,6 26,16 6,26z"></path>
-    </svg>
   </div>
 );
 
