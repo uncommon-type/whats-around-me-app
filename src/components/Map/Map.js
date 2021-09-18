a42635c6b0a6df0d1ed44160aa97149bedf2ecad
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-09-18 15:39:34 +0300

    Remove searchbar, update map styles

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -8,17 +8,15 @@ import { FetchContext } from '../../contexts/FetchContextProvider';
 import { GoogleContext } from '../../contexts/GoogleContextProvider';
 import { ShareLocationContext } from '../../contexts/ShareLocationContextProvider';
 
-import Header from '../Header';
-import SearchBar from '../SearchBar';
 import GeoLocationButton from '../GeoLocationButton';
 import Pin from './Pin';
-import DialogBody from '../Dialog/DialogBody';
 import UserMarker from './UserMarker';
+import DialogBody from '../Dialog/DialogBody';
+import mapStyle from './mapStyle.json';
 
 const Map = () => {
   const [locationDetails, setLocationDetails] = useState(null);
   const { data, centerCoords, setCenterCoords } = useContext(FetchContext);
-
   const {
     isWatchingLocation,
     userPosition,
@@ -27,7 +25,7 @@ const Map = () => {
     geolocationError,
   } = useContext(ShareLocationContext);
 
-  const { mapApiLoaded, handleApiLoaded } = useContext(GoogleContext);
+  const { handleApiLoaded } = useContext(GoogleContext);
   const { zoom } = data;
 
   const handlePinClick = (location) => {
@@ -50,73 +48,70 @@ const Map = () => {
   );
 
   return (
-    <>
-      <Header>
-        <SearchBar />
-      </Header>
+    <main>
+      <GeoLocationButton
+        handleSharing={handleSharing}
+        isWatchingLocation={isWatchingLocation}
+        handleStopSharing={handleStopSharing}
+        geolocationError={geolocationError}
+      />
 
-      <main>
-        <GeoLocationButton
-          handleSharing={handleSharing}
-          isWatchingLocation={isWatchingLocation}
-          handleStopSharing={handleStopSharing}
-          geolocationError={geolocationError}
-        />
-
-        <div className="app-content gap-top-700">
-          <div className="map">
-            <GoogleMapReact
-              bootstrapURLKeys={{
-                key: 'AIzaSyCz4t6JLp6xmiyAKnWa4MJnv16tH3EpOxA',
-                libraries: ['places'],
-              }}
-              center={centerCoords}
-              defaultZoom={zoom}
-              yesIWantToUseGoogleMapApiInternals
-              onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
-              onDragEnd={(map) => handleMapDrag(map)}
-            >
-              {data?.query?.pages?.length > 0 && mapApiLoaded
-                ? data.query.pages.map((location) => {
-                    const { coordinates, pageid: id } = location;
-                    return (
-                      <Pin
-                        key={id}
-                        lat={coordinates[0].lat}
-                        lng={coordinates[0].lon}
-                        imageUrl={
-                          location.thumbnail?.source ||
-                          `${process.env.PUBLIC_URL}/placeholder.png`
-                        }
-                        alt={location.thumbnail?.source ? location.title : ''}
-                        onClick={() => {
-                          handlePinClick(location);
-                        }}
-                      />
-                    );
-                  })
-                : null}
-
-              {userPosition ? (
-                <UserMarker lat={userPosition.lat} lng={userPosition.lng} />
-              ) : null}
-            </GoogleMapReact>
-            {locationDetails ? (
-              <Dialog
-                className="dialog"
-                onDismiss={handleCloseDialog}
-                aria-label="Location details"
-              >
-                <DialogBody
-                  onClick={handleCloseDialog}
-                  locationDetails={locationDetails}
-                />
-              </Dialog>
+      <div className="app-content gap-top-700 ">
+        <div className="map">
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: 'AIzaSyDReDgyRM1t9H2HncIec_v_zh2DeJGggT0',
+              libraries: ['places'],
+              id: '__googleMapsScriptId',
+              version: 'weekly',
+            }}
+            center={centerCoords}
+            defaultZoom={zoom}
+            yesIWantToUseGoogleMapApiInternals
+            onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+            onDragEnd={(map) => handleMapDrag(map)}
+            options={mapStyle}
+          >
+            {data?.query?.pages?.length > 0
+              ? data.query.pages.map((location) => {
+                  const { coordinates, pageid: id } = location;
+                  return (
+                    <Pin
+                      key={id}
+                      lat={coordinates[0].lat}
+                      lng={coordinates[0].lon}
+                      imageUrl={
+                        location.thumbnail?.source ||
+                        `${process.env.PUBLIC_URL}/placeholder.png`
+                      }
+                      alt={location.thumbnail?.source ? location.title : ''}
+                      onClick={() => {
+                        handlePinClick(location);
+                      }}
+                    />
+                  );
+                })
+              : null}
+            {userPosition ? (
+              <UserMarker lat={userPosition.lat} lng={userPosition.lng} />
             ) : null}
-          </div>
+          </GoogleMapReact>
+
+          {locationDetails ? (
+            <Dialog
+              className="dialog"
+              onDismiss={handleCloseDialog}
+              aria-label="Location details"
+            >
+              <DialogBody
+                onClick={handleCloseDialog}
+                locationDetails={locationDetails}
+              />
+            </Dialog>
+          ) : null}
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   );
 };
 
