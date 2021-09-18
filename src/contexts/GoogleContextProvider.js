dd644595efee8ad5fbaed3b0b44c506b8b0aeb44
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-09-18 15:45:48 +0300

    Remove mapApiLoaded

--- src/contexts/GoogleContextProvider.js
+++ src/contexts/GoogleContextProvider.js
@@ -5,22 +5,20 @@ export const GoogleContext = createContext();
 const GoogleContextProvider = ({ children }) => {
   const [mapInstance, setMapInstance] = useState(null);
   const [mapApi, setMapApi] = useState(null);
-  const [mapApiLoaded, setMapApiLoaded] = useState(false);
 
   const handleApiLoaded = (map, maps) => {
-    setMapApiLoaded(true);
     setMapInstance(map);
     setMapApi(maps);
   };
 
   const context = {
-    mapApiLoaded,
     handleApiLoaded,
     mapInstance,
     setMapInstance,
     mapApi,
     setMapApi,
   };
+
   return (
     <GoogleContext.Provider value={context}>{children}</GoogleContext.Provider>
   );
